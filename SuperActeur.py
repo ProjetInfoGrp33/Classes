ca6b65d3fe1f3d69fca3346529f5afc68cf95700
ines8299 <64084168+ines8299@users.noreply.github.com>
2020-04-21 15:52:52 +0200

    Update SuperActeur.py

--- SuperActeur.py
+++ SuperActeur.py
@@ -1,4 +1,11 @@
 class SuperActeur():
-  def __init__(self,id,mdp):
-    self.id=id
-    self.mdp=mdp
+  def __init__(self,statut):
+    self.statut=statut
+
+  def connexion(self):
+    pass
+  def deconnexion(self):
+    pass
+  def afficher_pays(self):
+    pass
+  
