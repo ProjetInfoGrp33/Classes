34c96ce53f5684490e387a05272d41c7b9facb04
justinkirion <64084594+justinkirion@users.noreply.github.com>
2020-04-22 10:09:24 +0200

    Update SuperActeur.py

--- SuperActeur.py
+++ SuperActeur.py
@@ -2,10 +2,23 @@ class SuperActeur():
   def __init__(self,statut):
     self.statut=statut
 
-  def connexion(self):
-    pass
-  def deconnexion(self):
-    pass
   def afficher_pays(self):
-    pass
+    print("Entrer le nom du pays à afficher :")
+    while True:
+      temp=0
+      value = input("> ")
+      for cle in dict_pays : 
+        if str(value)==cle :
+          print dict_pays[cle]
+          temp=1
+          break
+      if not temp :
+        print("Le pays n'existe pas. Voulez-vous recommencer ?")
+        print("[1] Oui")
+        print("[2] Non")
+        while True:
+          value2 = input("> ")
+          if value2 in ["1","Oui","oui","O","o"]:
+            afficher_pays(self)
+    return menu.open_menu.Menu(memory)
   
