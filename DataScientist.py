1fdef83d99003e9b4624c06f17c1eaec53c3b3c0
erwanguyader <64084369+erwanguyader@users.noreply.github.com>
2020-04-22 19:30:01 +0200

    Update DataScientist.py

--- DataScientist.py
+++ DataScientist.py
@@ -1,4 +1,4 @@
-Class DataScientist(Consultant, Classe_abstraite1):
+Class data_scientist(Consultant, Classe_abstraite1):
   def __init__(id_ds,motdepasse_ds, activite_ds):
     self.id_ds=id_ds
     self.motdepasse_ds=motdepasse_ds
