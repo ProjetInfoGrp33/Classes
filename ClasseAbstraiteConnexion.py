a9c6685038710c5c2216c8ace3cbf9b31baab668
justinkirion <64084594+justinkirion@users.noreply.github.com>
2020-04-22 10:41:25 +0200

    Update ClasseAbstraiteConnexion.py

--- ClasseAbstraiteConnexion.py
+++ ClasseAbstraiteConnexion.py
@@ -1,1 +1,42 @@
-Class AbsConnexion():
+Class AbsConnexion(SuperActeur):
+
+  def __init__(self,statut,activite):
+    self.statut=statut
+    self.activite=activite
+    
+  def connexion(self):
+    if self.activite==1:
+      print("Erreur : utilisateur déjà connecté :")
+      return menu.open_menu.Menu(memory)
+    print("Entrer votre identifiant:")
+    while True:
+      temp=0
+      value=input("> ")
+      for id in dict_compte:
+        if value==id:
+          temp=1
+      if not temp:
+        print("Identifiant inexistant, veuillez réessayer")
+        continue
+    print ("Entrer votre mot de passe :")
+    while True:
+      value2=input("> ")
+      if str(value2)==dict_compte[value][0] : 
+        print "Connexion réussie !"
+        self.activite=1
+      else :
+        print("Le mot de passe est incorrect. Voulez-vous recommencer ?")
+        print("[1] Oui")
+        print("[2] Non")
+        while True:
+          value2 = input("> ")
+          if value2 in ["1","Oui","oui","O","o"]:
+            break
+        print ("Entrer votre mot de passe :")
+        continue
+    return menu.open_menu.Menu(memory)
+  
+  def deconnexion(self):
+    print ("Au Revoir")
+    self.activite=0
+    
