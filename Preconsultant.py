e59d748aefc982936f96880366d1bce1f270aad5
justinkirion <64084594+justinkirion@users.noreply.github.com>
2020-04-27 12:40:29 +0200

    Update Preconsultant.py

--- Preconsultant.py
+++ Preconsultant.py
@@ -11,8 +11,9 @@ Class Preconsultant():
         print("[1] Oui")
         print("[2] Non")
         value = input("> ")
-        if value in ["1","Oui","oui","O","o"]:
-          continue
+        if value in ["2","Non","non","N","n"]:
+          break
+      else break
     print ("Entrer le numéro de l'information à corriger :")
     while True:
       num_info = input("> ")
@@ -20,8 +21,9 @@ Class Preconsultant():
       except ValueError : 
         print ("La réponse attendue est un nombre entier compris entre 0 et le nombre maximum d'informations")
         continue
-    print ("Entrer le contenu de la correction :")
-    correction = input("> ")
-    indice=len(dict_correction)
-    dict_correction[indice] = [pays, info, correction]
+      print ("Entrer le contenu de la correction :")
+      correction = input("> ")
+      indice=len(dict_correction)
+      dict_correction[indice] = [pays, info, correction]
+      break
     return menu.open_menu.Menu(memory)
