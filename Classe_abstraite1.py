054a5db7f8e09d0557a05713de5b27587e3c5535
ines8299 <64084168+ines8299@users.noreply.github.com>
2020-04-27 16:31:05 +0200

    Update Classe_abstraite1.py

--- Classe_abstraite1.py
+++ Classe_abstraite1.py
@@ -6,11 +6,14 @@ import numpy as np
 # A faire : menu pour qu'il choisisse dans les 4 actions possibles (un peu long sinon)
 # + tests unitaire à rajouter : lorsqu'on choisit critere dans la liste n entre 1 et 9
 
+from fonctions_intermediaires_stat import pays_dans_la_base , choix_n , choix_pays , choix_critere , choix_seuil , liste_criteres, liste_classes_age , liste_critere_donnee, valeurs_classes_age, liste_pays
+import matplotlib.pyplot as plt
+
 Class classe_abstraite1(classe_abstraite_connexion):
   def __init__(self,id=None,mdp=None,statut=None,activite=0):
     classe_abstraite_connexion.__init__(self,id,mdp,statut,activite)
   
-  def resume_informations(self,donnees): # créer un menu pour choisir dans les actions proposées
+    def resume_informations(self,donnees): # créer un menu pour choisir dans les actions proposées
         # n premiers/derniers pays pour 1 critere 
         # les pays dont critere depasse un seuil
         # tableau des classes d'ages (10 max)
@@ -22,116 +25,12 @@ Class classe_abstraite1(classe_abstraite_connexion):
         seuil(donnees)
         print("--------------------------")
         tableau_classes_age(donnees)
+    
+    def representation_graphique(self,donnees):
+        diagramme_barre(donnees)
+        print("--------------------------")
+        boxplot_age(donnees)
         
-
-
-
-# FONCTION INTERMEDIAIRES
-
-# test si le pays est dans la base
-def pays_dans_la_base(donnees,pays): 
-    try:
-        donnees[pays]
-    except KeyError:
-        print("Le pays ne se trouve pas dans la base, réessayer")
-        return(False)
-    else:
-        return(True)
-
-def choix_n(nmin=-10000,nmax=100000):
-    while True:
-        try:
-            n = int(input(" >"))
-        except ValueError:
-            print("Veuillez rentrer un entier svp")
-            continue
-        else:
-            break
-    return(n)
-    
- # choix d'une liste de pays
-def choix_pays(donnees,nmax=10000):
-    liste_pays=[]
-    i=1
-    Continuer = True
-    iter=0
-    while Continuer and iter<nmax:
-        iter+=1
-        print("Veuillez rentrer le nom du pays numéro {}".format(i))
-        pays_choisi = input("> ")
-        Test=pays_dans_la_base(donnees,pays_choisi) # on fait le test unitaire pour voir si le pays appartient à la base
-        if Test:
-            liste_pays.append(pays_choisi)
-            i +=1
-        else:
-            continue
-        print("Voulez vous rajouter un pays? (O/N)")
-        choice= input(' >')
-        if choice in ['O','Oui']:
-            pass
-        else:
-            Continuer=False
-    return(liste_pays)
-    
-# choix du critere parmis la liste
-def choix_critere(donnees): 
-    print("Veuillez choisir le critère dans la liste suivante")
-    indice=1
-    pays='France'
-    for i in donnees[pays].keys():
-        if i!= "Classes Age":
-            print('[{}]'.format(indice),i)
-            indice +=1
-    choice = int(input(" >"))
-    
-    critere = liste_criteres(donnees,'France')[choice-1]
-    return(critere)
-
-# choix du seuil pour un critere (on donne le min et le max pour aider l'utilisateur) 
-def choix_seuil(donnees,critere):
-    liste_critere = liste_critere_donnee(donnees,critere)
-    print("Veuillez taper le seuil (sans unité), les valeurs associées à {} se situe entre {} et {}".format(critere,min(liste_critere),max(liste_critere)))
-    while True:
-        try:
-            seuil = input(" >")
-            seuil=float(seuil)
-        except ValueError:
-            print("Erreur: veuillez taper un nombre")
-            continue
-        else:
-            break
-    return(seuil)
-        
-# liste des criteres["Population","Taux de Chomage" ...]
-def liste_criteres(donnees,pays): 
-    criteres=[]
-    for i in donnees[pays].keys():
-        if i!='Classes Age':
-            criteres.append(i)
-    return(criteres)
-    
-def liste_classes_age(donnees,pays):
-    resultat=[]
-    for i in donnees[pays]["Classes Age"].keys():
-        resultat.append(i)
-    return(resultat)
-    
- # liste des valeurs pour un critere donné (sans NA)
-def liste_critere_donnee(donnees,critere):
-    resultat=[]
-    for i in donnees.keys():
-        if donnees[i][critere] != None:
-            resultat.append(donnees[i][critere])
-    return(resultat)
-    
-# liste des pays où le critere choisi n'est pas NA
-def liste_pays(donnees,critere):
-    liste=[]
-    for i in donnees.keys():
-        if donnees[i][critere]!= None:
-            liste.append(i)
-    return(liste)
-
 
 ############### FONCTIONS FINALES
 def affichage_critere(donnees): # affiche criteres en un tableau pandas 
@@ -194,17 +93,40 @@ def seuil(donnees):
 
 def tableau_classes_age(donnees):
     print("Tableau des classes d'age pour 10 pays maximum")
-    liste_pays = choix_pays(donnees,10)
-    
+    #on choisit les 10 pays max
+    pays = choix_pays(donnees,10)
     # on crée un tableau où chaque ligne = un pays et colonnes = classes d'ages
-    tableau = []
-    classes = liste_classes_age(donnees,liste_pays[0])
-    for pays in liste_pays:
-        ligne=[]
-        for classe in classes:
-            ligne.append(donnees[pays]['Classes Age'][classe])
-        tableau.append(ligne)
-    dataframe= pandas.DataFrame(data=tableau, columns=classes,index=liste_pays)
+    tableau = valeurs_classes_age(donnees,pays)
+    #on convertie en data frame pour faciliter l'affichage
+    dataframe= pandas.DataFrame(data=tableau, columns=classes,index=pays)
     pandas.set_option("display.max_rows", None, "display.max_columns", None)
     print(dataframe)
+
+#diagramme en barre pour un critere donné
+def diagramme_barre(donnees):
+    print("Diagramme en barre pour un critère donné")
+    critere = choix_critere(donnees)
+    #on sort la liste avec valeurs du critere choisi (sans NA)
+    liste_critere = np.log(liste_critere_donnee(donnees,critere))
+
+    plt.hist(liste_critere, bins = 5, color = 'yellow',
+            edgecolor = 'red', rwidth = 0.8, log = False)
+    plt.ylabel('Effectif')
+    plt.xlabel('{} (échelle logarithmique)'.format(critere))
+    plt.title('Diagramme en barre')
+    plt.show()
+
+def boxplot_age(donnees):
+    print("Boxplot de la répartition de la population par classes d'age")
+    #liste des classes [0-10 years, ...]
+    classes = liste_classes_age(donnees)
+    n = len(classes) #nombre de classes
+    #tableau des valeurs : [france [22%,11%,50%,17%]],[]
+    valeurs = valeurs_classes_age(donnees)
+    # on convertie en dataframe
+    dataframe= pandas.DataFrame(data=valeurs, columns=classes)
+    #on sort le boxplot pour toutes les variables 
+    dataframe.boxplot(column=classes)
     
+    input("Tapez sur Entrée pour continuer")
+
