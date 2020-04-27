0cb272ae923184a9d8bc3153d59ad5a2b671beca
justinkirion <64084594+justinkirion@users.noreply.github.com>
2020-04-27 11:55:25 +0200

    Update SuperActeur.py

--- SuperActeur.py
+++ SuperActeur.py
@@ -2,10 +2,10 @@ class SuperActeur():
   def __init__(self,statut):
     self.statut=statut
     
-  def verifier_pays (pays): # pour verifier que les paus 
+  def verifier_pays (self, pays): # pour verifier que les paus 
     return dict_pays.has_key(pays)   
   
-  def ret_afficher_pays (self, pays, memory): # ce sera 
+  def ret_afficher_pays (self): # ce sera 
     print("Entrer le nom du pays à afficher :")
     pays = input("> ")
     if verifier_pays (pays):
@@ -14,16 +14,16 @@ class SuperActeur():
       return None
 
   def afficher_pays(self, memory):
-    
-    if ret_afficher_pays==None:
-      print("Le pays n'existe pas. Voulez-vous recommencer ?")
-      print("[1] Oui")
-      print("[2] Non")
-      value2 = input("> ")
-        if value2 in ["1","Oui","oui","O","o"]:
-          return afficher_pays(self)
-    else:
-      print(ret_afficher_pays)
+    while True: 
+      if ret_afficher_pays(self)==None:
+        print("Le pays n'existe pas. Voulez-vous recommencer ?")
+        print("[1] Oui")
+        print("[2] Non")
+        value2 = input("> ")
+          if value2 in ["1","Oui","oui","O","o"]:
+            continue
+      else:
+        print(ret_afficher_pays)
     return menu.open_menu.Menu(memory)
   
   
