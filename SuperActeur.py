1b8402d7350be1d9e5d4cf9b11e7842147b867bd
justinkirion <64084594+justinkirion@users.noreply.github.com>
2020-04-27 12:26:32 +0200

    Update SuperActeur.py

--- SuperActeur.py
+++ SuperActeur.py
@@ -5,7 +5,7 @@ class SuperActeur():
   def verifier_pays (self, pays): # pour verifier que les paus 
     return dict_pays.has_key(pays)   
   
-  def ret_afficher_pays (self, pays): # ce sera 
+  def ret_afficher_pays (self): # ce sera 
     print("Entrer le nom du pays à afficher (première lettre majuscule, puis lettres minuscules) :")
     pays = input("> ")
     if verifier_pays (self, pays):
