9b980f0f5c8fb86f8d5df866a18bf18ddfc8b318
ines8299 <64084168+ines8299@users.noreply.github.com>
2020-04-29 18:09:43 +0200

    Update Preconsultant.py

--- Preconsultant.py
+++ Preconsultant.py
@@ -1,32 +1,16 @@
-Class Preconsultant():
+from fonctions_intermediaires_stat import choix_critere , rentrer_valeur_critere, choix_pays
+
+class Preconsultant():
   def __init__(self):
-  
-  def proposer_correction(self,memory):
-    while True : 
-      print ("Entrer le nom du pays à corriger :")
-      temp=0
-      pays = input("> ")
-      if not dict_pays.has_key(pays):
-        print("Le pays n'existe pas. Voulez-vous recommencer ?")
-        print("[1] Oui")
-        print("[2] Non")
-        value = input("> ")
-        if value in ["2","Non","non","N","n"]:
-          break
-      else break
-    print ("Entrer le numéro de l'information à corriger :")
-    while True:
-      num_info = input("> ")
-      try : num_info=int(num_info) and 0<= num_info and num_info <= len(dict_pays[str(pays)])
-      except ValueError : 
-        print ("La réponse attendue est un nombre entier compris entre 0 et le nombre maximum d'informations")
-        continue
+      pass
+  def proposer_correction(self,memory,dict_pays,dict_correction):
+      pays = choix_pays(dict_pays)
+      print ("Choix du critere a modifier")
+      critere = choix_critere(dict_pays)
       print ("Entrer le contenu de la correction :")
-      correction = input("> ")
-      indice=0
-      for cle in dict_correction : 
-        indice=max(indice, cle)
-      indice+=1
-      dict_correction[indice] = [pays, info, correction]
-      break
-    return menu.open_menu.Menu(memory)
+      correction = rentrer_valeur_critere()
+      indice=1
+      nombre_corrections = len(dict_correction)
+      indice=max(indice, nombre_corrections)
+      dict_correction[indice] = [pays, critere, correction]
+      return memory  #menu.open_menu.Menu(memory)
