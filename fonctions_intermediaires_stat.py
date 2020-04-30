0e77a7e6a2915cd9f8f15850903ee03fad0e2dbd
ines8299 <64084168+ines8299@users.noreply.github.com>
2020-04-30 16:39:07 +0200

    Update fonctions_intermediaires_stat.py

--- fonctions_intermediaires_stat.py
+++ fonctions_intermediaires_stat.py
@@ -196,3 +196,53 @@ def rentrer_valeur_critere(nmin=None,nmax=None):
             break
     
     return(correction)
+    
+#choisit le nom d'un pays à ajouter
+def rentrer_pays(donnees):
+    while True: 
+        print("Veuillez rentrer le nom du pays que vous souhaitez créer:")
+        pays=input(" >")
+        if pays_dans_la_base(donnees,pays):
+            print("Le pays existe déjà. Voulez-vous recommencer ?")
+            print("[1] Oui")
+            print("[2] Non")
+            value2 = input("> ")
+            if value2 in ["2","Non","non","N","n"]:
+                pays=None
+                break
+            else:
+                continue
+        else:
+            break
+    return pays
+    
+    
+#afficher toutes les infos sur le pays choisi
+def afficher_infos(dict_pays,pays):
+    criteres=liste_criteres(dict_pays,pays)
+    classes= liste_classes_age(dict_pays)
+    for critere in criteres:
+        print(critere + " : " + str(dict_pays[pays][critere]))
+    print("Classes d'age (pourcentage de la population):")
+    for classe in classes:
+        print(classe + " : " + str(dict_pays[pays]['Classes Age'][classe]))
+
+#rentre les valeurs pour les classes d'ages pour un pays donnée 
+def rentrer_valeurs_classes_age(donnees,pays): #changer (ajouter) valeur pour les classes d'age
+    print("Classes d'ages (pourcentage dans la population, entre 0 et 1)")
+    classes= liste_classes_age(donnees)
+    while True:
+        somme=0
+        for classe in classes:
+            print("Veuillez rentrer la valeur de {} ".format(classe))
+            valeur = rentrer_valeur_critere(0,1)
+            donnees[pays]['Classes Age'][classe]=valeur
+            print("Valeur pour {} : {}".format(classe,valeur))
+            somme += valeur
+        if somme !=1:
+            print("La somme ne fait pas 1, veuillez retaper les valeurs")
+            continue
+        else:
+            break
+
+   
