f2c749f9d63157504e3a009b99238293ce32a33f
ines8299 <64084168+ines8299@users.noreply.github.com>
2020-05-01 11:58:51 +0200

    Update Geographe.py

--- Geographe.py
+++ Geographe.py
@@ -2,5 +2,5 @@ from Classe_abstraite2 import Classe_abstraite2
 
 class Geographe(Classe_abstraite2):
     def __init__(self,id=None,mdp=None,Activite=0, Statut='Geographe'):
-        Classe_abstraite2.__init__(id,mdp,Activite,Statut)
+        Classe_abstraite2.__init__(self,id,mdp,Activite,Statut)
 
