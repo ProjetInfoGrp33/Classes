eaf59ecf2922df29261cbdcc17a07168406a0ef0
ines8299 <64084168+ines8299@users.noreply.github.com>
2020-05-01 11:59:12 +0200

    Update DataScientist.py

--- DataScientist.py
+++ DataScientist.py
@@ -3,4 +3,4 @@ from Consultant import Consultant
  
 class DataScientist(Consultant, Classe_abstraite1):
   def __init__(self,id=None,mdp=None,activite=0,statut='DataScientist'):
-      Classe_abstraite1.__init__(id,mdp,statut,activite)
+      Classe_abstraite1.__init__(self,id,mdp,statut,activite)
