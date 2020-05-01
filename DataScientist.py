ed0fb7cbe973e441d3e078fb388b06042c94a8f3
justinkirion <64084594+justinkirion@users.noreply.github.com>
2020-05-01 14:12:59 +0200

    Update DataScientist.py

--- DataScientist.py
+++ DataScientist.py
@@ -1,6 +1,6 @@
-from Classe_abstraite1 import Classe_abstraite1
-from Consultant import Consultant
+from ClassesActeur.Classe_abstraite1 import Classe_abstraite1
+from ClassesActeur.Preconsultant import Preconsultant
  
-class DataScientist(Consultant, Classe_abstraite1):
+class DataScientist(Preconsultant, Classe_abstraite1):
   def __init__(self,id=None,mdp=None,activite=0,statut='DataScientist'):
       Classe_abstraite1.__init__(self,id,mdp,statut,activite)
