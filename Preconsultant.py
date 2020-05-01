cd0dc5b37479cde3a387a7c3fe458a33fef2ad1a
ines8299 <64084168+ines8299@users.noreply.github.com>
2020-05-01 14:42:14 +0200

    Update Preconsultant.py

--- Preconsultant.py
+++ Preconsultant.py
@@ -3,14 +3,11 @@ from ClassesActeur.fonctions_intermediaires_stat import choix_critere , rentrer_
 def __init__(self, statut='Consultant'):
       pass
       
-  def proposer_correction(self,memory,dict_pays,dict_correction):
+  def proposer_correction(self,memory,dict_pays,liste_correction):
       pays = choix_pays(dict_pays)
       print ("Choix du critere a modifier")
       critere = choix_critere(dict_pays)
       print ("Entrer le contenu de la correction :")
       correction = rentrer_valeur_critere()
-      indice=1
-      nombre_corrections = len(dict_correction)
-      indice=max(indice, nombre_corrections)
-      dict_correction[indice] = [pays, critere, correction]
+      liste_correction.append = [pays, critere, correction]
       return memory  #menu.open_menu.Menu(memory)
