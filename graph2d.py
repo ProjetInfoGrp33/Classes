ae562739e77c28b5704151cc6088ef7bf0dff7e1
erwanguyader <64084369+erwanguyader@users.noreply.github.com>
2020-05-02 14:32:31 +0200

    Update graph2d.py

--- graph2d.py
+++ graph2d.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 
-def graph2d(self, memory):
+def graph2d(self, dico_pays, memory):
   liste_info=["Superficie", "Population", "Croissance démographique", "Inflation", "Dette", "Taux de Chomage", "Taux de Dépenses Santé", 
               "Taux de Dépenses Education", "Taux de Dépenses Militaire"]
-  for i in range(liste_info):
+  for i in range(len(liste_info)):
     print("["+str(i+1)+"]"+liste_info[i])
   print("Quelle variable souhaitez-vous mettre en abscisse ?")
   indice_info_x=input(">")
-  info_x=liste_info[indice_info_x-1]
+  info_x=liste_info[int(indice_info_x)-1]
   x=[]
   for cle in dico_pays.keys():
     x.append(dico_pays[cle][info_x])
@@ -18,9 +18,9 @@ def graph2d(self, memory):
     
   for k in range(liste_info):
     print("["+str(k+1)+"]"+liste_info[k])
-  print("Quelle variable souhaitez-vous mettre en abscisse ?")
+  print("Quelle variable souhaitez-vous mettre en ordonné ?")
   indice_info_y=input(">")
-  info_y=liste_info[indice_info_y-1]
+  info_y=liste_info[int(indice_info_y)-1]
   y=[]
   for cle in dico_pays.keys():
     y.append(dico_pays[cle][info_y])
@@ -30,7 +30,7 @@ def graph2d(self, memory):
     y_selon_x.append(0)
   
   for m in range (len(y_selon_x)):
-    y_selon_x[m]=y[x_rangs_[i]]
+    y_selon_x[m]=y[rangs_x[m]]
   
   plt.scatter(x_croissant,y_selon_x)
   plt.title(info_x+" en fonction de " + info_y)
