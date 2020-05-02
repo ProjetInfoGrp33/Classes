ddef0c05da1efd0c369d9d8d843f234466441376
erwanguyader <64084369+erwanguyader@users.noreply.github.com>
2020-05-02 14:04:46 +0200

    Update graph2d.py

--- graph2d.py
+++ graph2d.py
@@ -1,3 +1,44 @@
+import matplotlib.pyplot as plt
 
 def graph2d(self, memory):
-  liste_info=["Superficie", "Population", "Croissance démographique", "Inflation", "Dette", "Taux de Chomage", "Taux de Dépenses Santé"
+  liste_info=["Superficie", "Population", "Croissance démographique", "Inflation", "Dette", "Taux de Chomage", "Taux de Dépenses Santé", 
+              "Taux de Dépenses Education", "Taux de Dépenses Militaire"]
+  for i in range(liste_info):
+    print("["+str(i+1)+"]"+liste_info[i])
+  print("Quelle variable souhaitez-vous mettre en abscisse ?")
+  indice_info_x=input(">")
+  info_x=liste_info[indice_info_x-1]
+  x=[]
+  for cle in dico_pays.keys():
+    x.append(dico_pays[cle][info_x])
+  x_croissant=sorted(x)
+  rangs_x=[]
+  for j in range(len(x)):
+    rangs_x.append(x_croissant.index(x[j]))
+    
+  for k in range(liste_info):
+    print("["+str(k+1)+"]"+liste_info[k])
+  print("Quelle variable souhaitez-vous mettre en abscisse ?")
+  indice_info_y=input(">")
+  info_y=liste_info[indice_info_y-1]
+  y=[]
+  for cle in dico_pays.keys():
+    y.append(dico_pays[cle][info_y])
+  y_selon_x=[]
+  
+  for l in range(len(y)):
+    y_selon_x.append(0)
+  
+  for m in range (len(y_selon_x)):
+    y_selon_x[m]=y[x_rangs_[i]]
+  
+  plt.scatter(x_croissant,y_selon_x)
+  plt.title(info_x+" en fonction de " + info_y)
+  
+  plt.xlabel(info_x)
+  plt.ylabel(info_y)
+  
+  plt.show()
+  
+  
+              
