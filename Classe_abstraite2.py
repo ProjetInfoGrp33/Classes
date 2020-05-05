93ba52d736918eedbb9f2b4d0e0fbfe2bcb1c4e1
justinkirion <64084594+justinkirion@users.noreply.github.com>
2020-05-05 09:12:56 +0200

    Update Classe_abstraite2.py

--- Classe_abstraite2.py
+++ Classe_abstraite2.py
@@ -7,10 +7,10 @@ class Classe_abstraite2(Classe_abstraite_connexion):
         """ La classe est une classe abstraite"""
         Classe_abstraite_connexion.__init__(self,identifiant,mdp,statut,activite)
     
-    def accepter_refuser_proposition(self,donnees,liste_correction,memory=" "):
+    def accepter_refuser_proposition(self,memory):
         """On accepte les propositions disponibles dans le dictionnaire des corrections"""
-        indice_proposition = choix_proposition(liste_correction)# l'utilisateur choisit la proposition qu'il veut
-        proposition = liste_correction[indice_proposition] # proposition correspondante
+        indice_proposition = choix_proposition(memory["Corrections"])# l'utilisateur choisit la proposition qu'il veut
+        proposition = memory["Corrections"][indice_proposition] # proposition correspondante
         print("Proposition choisie : ", proposition)
         
         print("Acceptez vous cette proposition? Oui : O, Non : N, Ne rien faire : touche Entrée")
@@ -20,13 +20,13 @@ class Classe_abstraite2(Classe_abstraite_connexion):
             pays = proposition[0]
             critere=proposition[1]
             valeur=proposition[2]
-            donnees[pays][critere]=valeur # on modifie le critere choisie par la modif
-            supprimer (indice_proposition, liste_correction) # on supprime la proposition du dictionnaire, "proposition" correspond à la clé de la proposition
+            memory["data"][pays][critere]=valeur # on modifie le critere choisie par la modif
+            supprimer (indice_proposition, memory["Corrections"]) # on supprime la proposition du dictionnaire, "proposition" correspond à la clé de la proposition
             
             print("Modification effectuée avec succès")
             
         elif rep_oui_non.upper() in ['NON','N']:
-            supprimer (indice_proposition, liste_correction) # on supprime la proposition du dictionnaire, "proposition" correspond à la clé de la proposition
+            supprimer (indice_proposition, memory["Corrections"]) # on supprime la proposition du dictionnaire, "proposition" correspond à la clé de la proposition
             print("Proposition effacée.")
         else:
             print("Opération annulée") # on touche à rien
@@ -34,50 +34,50 @@ class Classe_abstraite2(Classe_abstraite_connexion):
     
 
     
-    def ajouter_pays(self,donnees,memory): # 
+    def ajouter_pays(self,memory): # 
         #on rentre le nom du pays a ajouter
-        nom_pays=rentrer_pays(donnees)
+        nom_pays=rentrer_pays(memory["data"])
         if nom_pays is None:
             print("Procédure annulée")
         else:  
-            criteres = liste_criteres(donnees)
-            donnees[nom_pays]={}
+            criteres = liste_criteres(memory["data"])
+            memory["data"][nom_pays]={}
             # on demande la valeur a rentrer pour chaque critere
             for i in criteres:
                 print("Veuillez rentrer la valeur de {} ".format(i))
                 valeur = rentrer_valeur_critere()
-                donnees[nom_pays][i]=valeur
+                memory["data"][nom_pays][i]=valeur
                 print("Valeur pour {} : {}".format(i,valeur))
             # cas particulier des classes d'age
-            donnees[nom_pays]['Classes Age']={}
-            rentrer_valeurs_classes_age(donnees,nom_pays)
+            memory["data"][nom_pays]['Classes Age']={}
+            rentrer_valeurs_classes_age(memory["data"],nom_pays)
             # est-ce qu'on fait confirmer à chaque saisie?
         return memory
     
     
   
-    def modifier_pays(self,dico_pays,memory):
+    def modifier_pays(self,memory):
         print("Quel pays souhaitez-vous modifier les informations ?")
-        nom_pays = choix_pays(dico_pays)
+        nom_pays = choix_pays(memory["data"])
         if nom_pays is None:
             print("Procédure annulée")
         else:
             print("Quelle information souhaitez vous modifier?")
-            critere_a_modifier= choix_critere(dico_pays,True)
+            critere_a_modifier= choix_critere(memory["data"],True)
             if critere_a_modifier !='Classes Age':
                 print("Voici l'information actuelle que vous souhaitez modifier.")
-                print(dico_pays[nom_pays][critere_a_modifier])
+                print(memory["data"][nom_pays][critere_a_modifier])
                 print("Veuillez saisir l'information qui remplaçera l'information ci-dessus")
                 new_info=rentrer_valeur_critere()
-                dico_pays[nom_pays][critere_a_modifier]=new_info
+                memory["data"][nom_pays][critere_a_modifier]=new_info
             else:
                 print("Voici l'information actuelle que vous souhaitez modifier.")
-                tableau=valeurs_classes_age(dico_pays,[nom_pays])
-                classes= liste_classes_age(dico_pays)
+                tableau=valeurs_classes_age(memory["data"],[nom_pays])
+                classes= liste_classes_age(memory["data"])
                 dataframe= pandas.DataFrame(data=tableau, columns=classes,index=[nom_pays])
                 pandas.set_option("display.max_rows", None, "display.max_columns", None)
                 print(dataframe)                
-                rentrer_valeurs_classes_age(dico_pays,nom_pays)
+                rentrer_valeurs_classes_age(memory["data"],nom_pays)
             print("L'information " + critere_a_modifier + " du pays " + nom_pays + " a été modifié")
         return memory
     
