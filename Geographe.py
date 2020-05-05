54111974f7e024e438c892bb74adae71756f1cc1
justinkirion <64084594+justinkirion@users.noreply.github.com>
2020-05-05 10:25:29 +0200

    Update Geographe.py

--- Geographe.py
+++ Geographe.py
@@ -1,6 +1,6 @@
 from ClassesActeur.Classe_abstraite2 import Classe_abstraite2
 
 class Geographe(Classe_abstraite2):
-    def __init__(self,id=None,mdp=None,Activite=0, Statut='Geographe'):
-        Classe_abstraite2.__init__(self,id,mdp,Activite,Statut)
+    def __init__(self,id=None,mdp=None, Statut='Geographe'):
+        Classe_abstraite2.__init__(self,id,mdp,Statut)
 
