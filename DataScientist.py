c89cf25ce993792ddc11aa0537a5ed334c80a4e4
justinkirion <64084594+justinkirion@users.noreply.github.com>
2020-05-05 10:27:53 +0200

    Update DataScientist.py

--- DataScientist.py
+++ DataScientist.py
@@ -3,4 +3,4 @@ from ClassesActeur.Preconsultant import Preconsultant
  
 class DataScientist(Preconsultant, Classe_abstraite1):
   def __init__(self,id=None,mdp=None,activite=0,statut='DataScientist'):
-      Classe_abstraite1.__init__(self,id,mdp,statut,activite)
+      Classe_abstraite1.__init__(self,id,mdp,statut)
